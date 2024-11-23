a14b030b8147807fa170a4eb4ae27325362b4041
Pedro Bueno <pedrolbb.e@gmail.com>
2024-11-23 16:53:03 -0300

    feat(CountryDetails): add dynamic route for Country details

---
+++ client/src/app/country/[countryCode]/page.tsx
@@ -0,0 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+"use client";
+
+import { withQueryClient } from "@/HOCs/withQueryClient";
+import { useFetchCountryInfo } from "@/hooks/useFetchCountryInfo";
+import { useParams } from "next/navigation";
+
+function CountryDetails() {
+  const params = useParams();
+  const countryCode = params.countryCode;
+
+  const {
+    data: country,
+    isLoading,
+    error,
+  } = useFetchCountryInfo(countryCode as string);
+
+  if (isLoading) return <div>Loading...</div>;
+  if (error) return <div>Error loading country details</div>;
+
+  return (
+    <div>
+      <h1>{country.name}</h1>
+      <p>Country Code: {country.countryCode}</p>
+    </div>
+  );
+}
+
+export default withQueryClient(CountryDetails);
