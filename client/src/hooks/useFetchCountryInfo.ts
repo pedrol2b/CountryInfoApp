b754cecc077c84ad29c308ab43f1e551ee31e07e
Pedro Bueno <pedrolbb.e@gmail.com>
2024-11-23 16:52:24 -0300

    feat(hooks): add useFetchCountryInfo query

---
+++ client/src/hooks/useFetchCountryInfo.ts
@@ -0,0 +1,17 @@
+"use client";
+
+import { API } from "@/services/API";
+import { useQuery } from "react-query";
+
+const api = API.getInstance().getAPIClient();
+
+const fetchCountryInfo = async (countryCode: string) => {
+  const { data } = await api.get(`/api/country-info/${countryCode}`);
+  return data;
+};
+
+export const useFetchCountryInfo = (countryCode: string) => {
+  return useQuery(["country-info", countryCode], () =>
+    fetchCountryInfo(countryCode)
+  );
+};
