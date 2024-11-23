dfb024281ff393906af64c3a647d599d04801618
Pedro Bueno <pedrolbb.e@gmail.com>
2024-11-23 19:24:50 -0300

    chore: typed response from useFetchCountryInfo query

--- client/src/hooks/useFetchCountryInfo.ts
+++ client/src/hooks/useFetchCountryInfo.ts
@@ -5,8 +5,24 @@ import { useQuery } from 'react-query'
 
 const api = API.getInstance().getAPIClient()
 
+type CountryInfoResponse = {
+  countryName: string
+  flag: string
+  borders: Array<{
+    commonName: string
+    officialName: string
+    countryCode: string
+    region: string
+    borders: unknown
+  }>
+  population: Array<{
+    year: number
+    value: number
+  }>
+}
+
 const fetchCountryInfo = async (countryCode: string) => {
-  const { data } = await api.get(`/api/country-info/${countryCode}`)
+  const { data } = await api.get<CountryInfoResponse>(`/api/country-info/${countryCode}`)
   return data
 }
 
